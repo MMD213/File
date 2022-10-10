fca7b42c9df35703fac64f7fa79aadc77398cd2b
Макаров Михаил <58745072+MMD213@users.noreply.github.com>
2022-10-10 15:48:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args){
         try {
-            Number n = new Number("C:\\Users\\mihai\\OneDrive\\Desktop\\nimbz.xlsx", "C:\\Users\\mihai\\OneDrive\\Desktop\\dzd.txt");
+            Num n = new Num("C:\\Users\\mihai\\OneDrive\\Desktop\\nimbz.xlsx", "C:\\Users\\mihai\\OneDrive\\Desktop\\dzd.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
